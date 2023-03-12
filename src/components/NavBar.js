c025352251ec6718c2478586e17b43d42af826bc
Brian Wong <brianw7218@gmail.com>
2023-03-11 22:20:56 -0800

    Create preliminary structure for navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,25 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
     <div>
       {
         // Order: Link to Home, 3 sprites of the Pokemon to find, timer as the very middle, link to leaderboard
       }
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+      <div>
+        <img />
+        <img />
+        <img />
+      </div>
+      <div>
+        <p>Time: </p>
+      </div>
+      <Link to="/leaderboard">
+        <button>Leaderboard</button>
+      </Link>
     </div>
   );
 };
