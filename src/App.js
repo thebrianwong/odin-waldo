713eee19d8c68b0698bd707485ef7cf5044a9df6
Brian Wong <brianw7218@gmail.com>
2023-03-11 22:11:15 -0800

    Install router package and link homepage to app router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
-function App() {}
+import { Route, Router, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
 
 export default App;
