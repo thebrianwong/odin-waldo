b2c96b7d38393cbadcb9ec3c46d6d66658c2c133
Brian Wong <brianw7218@gmail.com>
2023-03-14 21:03:12 -0700

    Change placeholder names to get rid of same key error

--- src/gameData.js
+++ src/gameData.js
@@ -1,5 +1,5 @@
 export const gameData = {
-  version1: ["Pikachu", "Pikachu", "Pikachu"],
-  version2: ["Pikachu", "Pikachu", "Pikachu"],
-  version3: ["Pikachu", "Pikachu", "Pikachu"],
+  version1: ["Pikachu1", "Pikachu2", "Pikachu3"],
+  version2: ["Pikachu4", "Pikachu5", "Pikachu6"],
+  version3: ["Pikachu7", "Pikachu8", "Pikachu9"],
 };
