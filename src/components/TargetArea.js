4d1523dc339de2263c596dff516917bb43c25968
Brian Wong <brianw7218@gmail.com>
2023-03-15 22:07:39 -0700

    Ensure that the target area never crosses out of the image's left and right borders, and change border value to account for borders being only accounted for half of the time

--- src/components/TargetArea.js
+++ src/components/TargetArea.js
@@ -7,30 +7,35 @@ const TargetArea = ({
   const AREA_HEIGHT = 50;
   const AREA_WIDTH = 50;
   const AREA_RADIUS = 25;
-  const AREA_BORDER = 2;
+  const AREA_HALF_BORDER = 1;
   const LEEWAY_MARGIN = 5;
   const normalizeYCoordinate = () => {
     if (imagePosition.y + AREA_RADIUS > imageDimensions.height) {
       return (
-        imageBorders.bottom - 2 * AREA_RADIUS - AREA_BORDER - LEEWAY_MARGIN
+        imageBorders.bottom - 2 * AREA_RADIUS - AREA_HALF_BORDER - LEEWAY_MARGIN
       );
     } else if (imagePosition.y - AREA_RADIUS < 0) {
-      return imageBorders.top + AREA_BORDER + LEEWAY_MARGIN;
+      return imageBorders.top + AREA_HALF_BORDER + LEEWAY_MARGIN;
     }
     return clickPosition.y - AREA_RADIUS;
   };
   const normalizeXCoordinate = () => {
-    // if (clientPosition.x + MENU_WIDTH > VIEWPORT_WIDTH - LEEWAY_MARGIN) {
-    //   return clickPosition.x - MENU_WIDTH;
-    // }
-    return clickPosition.x;
+    if (imagePosition.x + AREA_RADIUS > imageDimensions.width) {
+      return (
+        imageBorders.right - 2 * AREA_RADIUS - AREA_HALF_BORDER - LEEWAY_MARGIN
+      );
+    } else if (imagePosition.x - AREA_RADIUS < 0) {
+      return imageBorders.left + AREA_HALF_BORDER + LEEWAY_MARGIN;
+    }
+    return clickPosition.x - AREA_RADIUS - AREA_HALF_BORDER;
   };
+  normalizeXCoordinate();
   return (
     <div
       style={{
         position: "absolute",
         top: normalizeYCoordinate(),
-        left: clickPosition.x - 25,
+        left: normalizeXCoordinate(),
         height: `${AREA_HEIGHT}px`,
         width: `${AREA_WIDTH}px`,
         border: "dashed 2px red",
