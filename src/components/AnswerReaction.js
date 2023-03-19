5db149120e9d734575c0a547f09a5a509ee8a2ab
Brian Wong <brianw7218@gmail.com>
2023-03-18 21:37:42 -0700

    Style the answer reaction component

--- src/components/AnswerReaction.js
+++ src/components/AnswerReaction.js
@@ -1,16 +1,37 @@
 const AnswerReaction = ({ isCorrect, clickPosition }) => {
+  const bgColor = isCorrect ? "rgba(116,255,98,0.78)" : "rgba(255,98,116,0.78)";
+  const brdColor = isCorrect ? "green" : "red";
   return (
     <div
       style={{
         position: "absolute",
         top: clickPosition.y,
         left: clickPosition.x,
+        height: "100px",
+        width: "100px",
+        backgroundColor: bgColor,
+        borderStyle: "solid",
+        borderWidth: "5px",
+        borderColor: brdColor,
+        borderRadius: "8px",
       }}
     >
       {isCorrect ? (
-        <img src={require("../assets/images/correct.png")} />
+        <img
+          style={{
+            height: "100px",
+            width: "100px",
+          }}
+          src={require("../assets/images/correct.png")}
+        />
       ) : (
-        <img src={require("../assets/images/incorrect.png")} />
+        <img
+          style={{
+            height: "100px",
+            width: "100px",
+          }}
+          src={require("../assets/images/incorrect.png")}
+        />
       )}
     </div>
   );
