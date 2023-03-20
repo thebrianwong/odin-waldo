4baed47297708bd640b29f219262e5e510ddf5f1
Brian Wong <brianw7218@gmail.com>
2023-03-19 18:09:55 -0700

    Add difficulty indicators for each game version card

--- src/components/GameVersionCard.js
+++ src/components/GameVersionCard.js
@@ -1,9 +1,19 @@
 import PokemonNameList from "./PokemonNameList";
 
 const GameVersionCard = ({ gameData, gameVersion, chooseGameVersion }) => {
+  const difficultyLabel = () => {
+    if (gameVersion === "version1") {
+      return "Normal";
+    } else if (gameVersion === "version2") {
+      return "Hard";
+    } else {
+      return "Weird";
+    }
+  };
   return (
     <button onClick={() => chooseGameVersion(gameVersion)}>
       <img />
+      <p>{difficultyLabel()}</p>
       <PokemonNameList gameData={gameData} gameVersion={gameVersion} />
     </button>
   );
