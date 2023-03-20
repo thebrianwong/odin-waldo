c00c3aaca0ca8680b87da98ec5346f4baf45cf74
Brian Wong <brianw7218@gmail.com>
2023-03-20 16:06:25 -0700

    Create the leaderboard page, displaying data from Firebase formatted appropriately and with the ability to change difficulty to see different scores

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,9 +1,24 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Leaderboard = ({ leaderboardData, formatTime }) => {
   const [gameVersion, setGameVersion] = useState("version1");
+  const difficultyLabel = () => {
+    if (gameVersion === "version1") {
+      return "Normal";
+    } else if (gameVersion === "version2") {
+      return "Hard";
+    } else {
+      return "Weird";
+    }
+  };
+  const sortedLeaderboardData = () => {
+    return leaderboardData[gameVersion].sort((a, b) => {
+      return a.score - b.score;
+    });
+  };
   return (
-    <>
+    <div>
       {
         // Top left is title, indicating which game option
         // Top right is a drop down menu to pick between game options
@@ -11,7 +26,53 @@ const Leaderboard = ({ leaderboardData, formatTime }) => {
         // Below is the main contents of the leaderboard scores
         // Each record has name, game time duration, and favorite Pokemon
       }
-    </>
+      <div>
+        <Link to="/">
+          <img />
+        </Link>
+        <div>
+          <div>
+            <h1>{difficultyLabel()}</h1>
+            <label htmlFor="difficulty">
+              Change difficulty: {""}
+              <select
+                onChange={(e) => setGameVersion(e.target.value)}
+                name="difficulty"
+                id="difficulty"
+              >
+                <option value="version1">Normal</option>
+                <option value="version2">Hard</option>
+                <option value="version3">Weird</option>
+              </select>
+            </label>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Rank</th>
+                <th scope="col">Player Name</th>
+                <th scope="col">Time</th>
+                <th scope="col">Favorite Pokemon</th>
+                <th scope="col">Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sortedLeaderboardData().map((entry, index) => {
+                return (
+                  <tr key={index}>
+                    <td scope="row">#{index + 1}</td>
+                    <td>{entry.name}</td>
+                    <td>{formatTime(entry.score)}</td>
+                    <td>{entry.favoritePokemon}</td>
+                    <td>{entry.timeStamp}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   );
 };
 
