f0299e3deef7017a03be233bb478f7f3ec0d41bc
Brian Wong <brianw7218@gmail.com>
2023-03-26 15:59:03 -0700

    Add tests for game page

--- src/tests/pages/Game.test.js
+++ src/tests/pages/Game.test.js
@@ -75,7 +75,7 @@ test("The page renders", () => {
     <Game
       gameData={gameData.version1}
       gameVersion="version1"
-      validationData={validationData}
+      validationData={validationData.version1}
       formatTime={mockFunction}
       submitScore={mockFunction}
     />,
@@ -94,7 +94,7 @@ test("The image can be clicked to display the target area and dropdown menu", ()
     <Game
       gameData={gameData.version1}
       gameVersion="version1"
-      validationData={validationData}
+      validationData={validationData.version1}
       formatTime={mockFunction}
       submitScore={mockFunction}
     />,
@@ -110,4 +110,88 @@ test("The image can be clicked to display the target area and dropdown menu", ()
   const dropdownElement = screen.getByTestId("dropdown-menu");
   expect(targetArea).toBeInTheDocument();
   expect(dropdownElement).toBeInTheDocument();
+});
+
+test("The target area and dropdown menu disappear when choosing an option", () => {
+  render(
+    <Game
+      gameData={gameData.version1}
+      gameVersion="version1"
+      validationData={validationData.version1}
+      formatTime={mockFunction}
+      submitScore={mockFunction}
+    />,
+    { wrapper: BrowserRouter }
+  );
+  const image = screen.getByAltText(
+    "A compilation of all Pokemon released up to Generation 4."
+  );
+  act(() => {
+    userEvent.click(image, { clientX: 400, clientY: 400 });
+  });
+  act(() => {
+    userEvent.click(image, { clientX: 400, clientY: 300 });
+  });
+  const targetArea = screen.queryByTestId("target-area");
+  const dropdownElement = screen.queryByTestId("dropdown-menu");
+  expect(targetArea).toBeNull();
+  expect(dropdownElement).toBeNull();
+});
+
+test("The dropdown menu options can be picked, causing the target area and dropdown menu to disappear", () => {
+  render(
+    <Game
+      gameData={gameData.version1}
+      gameVersion="version1"
+      validationData={validationData.version1}
+      formatTime={mockFunction}
+      submitScore={mockFunction}
+    />,
+    { wrapper: BrowserRouter }
+  );
+  const image = screen.getByAltText(
+    "A compilation of all Pokemon released up to Generation 4."
+  );
+  act(() => {
+    userEvent.click(image, { clientX: 400, clientY: 400 });
+  });
+  const dropdownElement = screen.getByTestId("dropdown-menu");
+  const pokemonOptions = within(dropdownElement).getAllByRole("listitem");
+  expect(pokemonOptions.length).toBe(3);
+  const mudkipName = within(pokemonOptions[0]).getByText("Mudkip");
+  expect(mudkipName).toBeInTheDocument();
+  act(() => {
+    userEvent.click(pokemonOptions[0]);
+  });
+  const targetArea = screen.queryByTestId("target-area");
+  const unrenderedDropdownElement = screen.queryByTestId("dropdown-menu");
+  expect(targetArea).toBeNull();
+  expect(unrenderedDropdownElement).toBeNull();
+  expect(mudkipName).not.toBeInTheDocument();
+});
+
+test("The answer reaction appears when a dropdown menu option is picked", () => {
+  render(
+    <Game
+      gameData={gameData.version1}
+      gameVersion="version1"
+      validationData={validationData.version1}
+      formatTime={mockFunction}
+      submitScore={mockFunction}
+    />,
+    { wrapper: BrowserRouter }
+  );
+  const image = screen.getByAltText(
+    "A compilation of all Pokemon released up to Generation 4."
+  );
+  act(() => {
+    userEvent.click(image, { clientX: 800, clientY: 800 });
+  });
+  const dropdownElement = screen.getByTestId("dropdown-menu");
+  const pokemonOptions = within(dropdownElement).getAllByRole("listitem");
+  act(() => {
+    userEvent.click(pokemonOptions[0]);
+  });
+  const answerReaction = screen.getByTestId("answer-reaction");
+  expect(answerReaction).toBeInTheDocument();
 });
