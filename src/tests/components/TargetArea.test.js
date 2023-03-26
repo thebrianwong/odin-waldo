89d331bfbb5d06d2d6ff982751dac24c1a3739b1
Brian Wong <brianw7218@gmail.com>
2023-03-25 17:39:39 -0700

    Add more tests for target area

--- src/tests/components/TargetArea.test.js
+++ src/tests/components/TargetArea.test.js
@@ -29,3 +29,45 @@ test("The target area does not cross out of the image when clicked near the imag
   const style = window.getComputedStyle(targetArea);
   expect(style.top).toBe("2941px");
 });
+
+test("The target area does not cross out of the image when clicked near the image top edge", () => {
+  render(
+    <TargetArea
+      imagePosition={{ x: 500, y: 5 }}
+      clickPosition={{ x: 500, y: 500 }}
+      imageBorder={{ top: 0, right: 2000, bottom: 3000, left: 0 }}
+      gameData={{ imageDimensions: { width: 1600, height: 2560 } }}
+    />
+  );
+  const targetArea = screen.getByTestId("target-area");
+  const style = window.getComputedStyle(targetArea);
+  expect(style.top).toBe("7px");
+});
+
+test("The target area does not cross out of the image when clicked near the image right edge", () => {
+  render(
+    <TargetArea
+      imagePosition={{ x: 3000, y: 500 }}
+      clickPosition={{ x: 500, y: 500 }}
+      imageBorder={{ top: 0, right: 2000, bottom: 3000, left: 0 }}
+      gameData={{ imageDimensions: { width: 1600, height: 2560 } }}
+    />
+  );
+  const targetArea = screen.getByTestId("target-area");
+  const style = window.getComputedStyle(targetArea);
+  expect(style.left).toBe("1941px");
+});
+
+test("The target area does not cross out of the image when clicked near the image left edge", () => {
+  render(
+    <TargetArea
+      imagePosition={{ x: 5, y: 500 }}
+      clickPosition={{ x: 500, y: 500 }}
+      imageBorder={{ top: 0, right: 2000, bottom: 3000, left: 0 }}
+      gameData={{ imageDimensions: { width: 1600, height: 2560 } }}
+    />
+  );
+  const targetArea = screen.getByTestId("target-area");
+  const style = window.getComputedStyle(targetArea);
+  expect(style.left).toBe("7px");
+});
