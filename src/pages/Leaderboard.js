2c240bec2a522f78ad738c23d832f9038c9096ba
Brian Wong <brianw7218@gmail.com>
2023-03-27 22:20:04 -0700

    Style the leaderboard page

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -25,59 +25,57 @@ const Leaderboard = ({ leaderboardData, initialGameVersion, formatTime }) => {
   };
 
   return (
-    <div data-testid="leaderboard">
-      <div>
-        <Link to="/">
-          <img
-            className="pokemon-logo"
-            src={require("../assets/images/misc/logo.png")}
-            alt="The original Pokemon logo with a styled font, dark blue outline, and yellow fill."
-          />
-        </Link>
-        <main>
-          <div>
-            <h1>{difficultyLabel()}</h1>
-            <label htmlFor="difficulty">
-              Change difficulty: {""}
-              <select
-                data-testid="select-menu"
-                onChange={(e) => setGameVersion(e.target.value)}
-                name="difficulty"
-                id="difficulty"
-                value={gameVersion}
-              >
-                <option value="version1">Normal</option>
-                <option value="version2">Hard</option>
-                <option value="version3">Weird</option>
-              </select>
-            </label>
-          </div>
-          <table>
-            <thead>
-              <tr>
-                <th scope="col">Rank</th>
-                <th scope="col">Trainer Name</th>
-                <th scope="col">Time</th>
-                <th scope="col">Favorite Pokemon</th>
-                <th scope="col">Date</th>
-              </tr>
-            </thead>
-            <tbody data-testid="table-body">
-              {sortedLeaderboardData().map((entry, index) => {
-                return (
-                  <tr key={index}>
-                    <td>#{index + 1}</td>
-                    <td>{entry.name}</td>
-                    <td>{formatTime(entry.score)}</td>
-                    <td>{entry.favoritePokemon}</td>
-                    <td>{entry.timeStamp}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </main>
-      </div>
+    <div className="leaderboard-page" data-testid="leaderboard">
+      <Link to="/">
+        <img
+          className="pokemon-logo"
+          src={require("../assets/images/misc/logo.png")}
+          alt="The original Pokemon logo with a styled font, dark blue outline, and yellow fill."
+        />
+      </Link>
+      <main className="leaderboard-contents">
+        <div className="leaderboard-difficulty-container">
+          <h1>{difficultyLabel()}</h1>
+          <label htmlFor="difficulty">
+            Change difficulty: {""}
+            <select
+              data-testid="select-menu"
+              onChange={(e) => setGameVersion(e.target.value)}
+              name="difficulty"
+              id="difficulty"
+              value={gameVersion}
+            >
+              <option value="version1">Normal</option>
+              <option value="version2">Hard</option>
+              <option value="version3">Weird</option>
+            </select>
+          </label>
+        </div>
+        <table className="leaderboard-score-table">
+          <thead>
+            <tr>
+              <th scope="col">Rank</th>
+              <th scope="col">Trainer Name</th>
+              <th scope="col">Time</th>
+              <th scope="col">Favorite Pokemon</th>
+              <th scope="col">Date</th>
+            </tr>
+          </thead>
+          <tbody data-testid="table-body">
+            {sortedLeaderboardData().map((entry, index) => {
+              return (
+                <tr key={index}>
+                  <td>#{index + 1}</td>
+                  <td>{entry.name}</td>
+                  <td>{formatTime(entry.score)}</td>
+                  <td>{entry.favoritePokemon}</td>
+                  <td>{entry.timeStamp}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 };
