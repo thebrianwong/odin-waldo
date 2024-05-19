c21ed4928d6b9e741a8937d4321fc6c91759b82b
Brian Wong <brianw7218@gmail.com>
2024-05-18 22:30:55 -0700

    Remove test code

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,6 @@ function App() {
         leaderboardWebsocket.addEventListener(
           "message",
           async (newLeaderboardData) => {
-            console.log(newLeaderboardData);
             const parsedLeaderboardData: LeaderboardTotal = await JSON.parse(
               newLeaderboardData.data
             );
