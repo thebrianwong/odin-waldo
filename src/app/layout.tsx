281e77ef753080d777fa0ec41fea047b0cd2b011
Brian Wong <brianw7218@gmail.com>
2024-06-27 22:06:12 -0700

    Remove unnecessary head tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, minimum-scale=1"
-        />
         <meta name="theme-color" content="#000000" />
         <meta
           name="description"
