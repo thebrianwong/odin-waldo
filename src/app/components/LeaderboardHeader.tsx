5ba2de8bbfe694c9bbd13ba7c756b68bf021b310
Brian Wong <brianw7218@gmail.com>
2024-07-01 22:17:31 -0700

    Create a client component to change game versions in the leaderboard page

---
+++ src/app/components/LeaderboardHeader.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useSearchParams, useRouter } from "next/navigation";
+import { ChangeEvent } from "react";
+
+export default function LeaderboardHeader() {
+  const router = useRouter();
+  const params = useSearchParams();
+  const gameVersion = params?.get("gameVersion");
+
+  const difficultyLabel = () => {
+    if (gameVersion === "hard") {
+      return "Hard";
+    } else if (gameVersion === "weird") {
+      return "Weird";
+    } else {
+      return "Normal";
+    }
+  };
+
+  const changeSearchParams = (newValue: string) => {
+    const params = new URLSearchParams();
+    params.set("gameVersion", newValue);
+    router.push(`/leaderboard?${params.toString()}`);
+  };
+
+  return (
+    <div className="leaderboard-difficulty-container">
+      <h1>{difficultyLabel()}</h1>
+      <label
+        className="leaderboard-difficulty-select-label"
+        htmlFor="difficulty"
+      >
+        Change difficulty: {""}
+        <select
+          className="leaderboard-difficulty-select"
+          data-testid="select-menu"
+          onChange={(e: ChangeEvent) => {
+            changeSearchParams((e.target as HTMLOptionElement).value);
+          }}
+          name="difficulty"
+          id="difficulty"
+          value={gameVersion || "normal"}
+        >
+          <option value="normal">Normal</option>
+          <option value="hard">Hard</option>
+          <option value="weird">Weird</option>
+        </select>
+      </label>
+    </div>
+  );
+}
