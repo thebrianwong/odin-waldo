2bcf7ffb5f8368d696d36617911943d09b1efbd2
Brian Wong <brianw7218@gmail.com>
2024-07-03 21:44:34 -0700

    Show the loading pokeball while fetching leaderboard data

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import logo from "../../../public/assets/images/misc/logo.png";
 import LeaderboardHeader from "../../components/LeaderboardHeader";
 import LeaderboardContents from "src/components/LeaderboardContents";
+import { Suspense } from "react";
+import LoadingPokeball from "src/components/LoadingPokeball/LoadingPokeball";
 
 interface LeaderboardPageProps {
   searchParams: {
@@ -13,37 +15,39 @@ export default function LeaderboardPage({
   searchParams,
 }: LeaderboardPageProps) {
   return (
-    <div className="leaderboard-page" data-testid="leaderboard">
-      <Link className="nav-button-container" href="/">
-        <button className="nav-button">Home</button>
-      </Link>
-      <header>
-        <a href="https://www.youtube.com/watch?v=fCkeLBGSINs">
-          <img
-            className="pokemon-logo"
-            src={logo.src}
-            alt="The original Pokemon logo with a styled font, dark blue outline, and yellow fill."
-          />
-        </a>
-      </header>
-      <main className="leaderboard-contents">
-        <LeaderboardHeader />
-        <div className="leaderboard-score-table-container">
-          <table className="leaderboard-score-table">
-            <thead>
-              <tr>
-                <th scope="col">Rank</th>
-                <th scope="col">Trainer Name</th>
-                <th scope="col">Time</th>
-                <th scope="col">Favorite Pokemon</th>
-                <th scope="col">Date</th>
-              </tr>
-            </thead>
-            {/* @ts-expect-error Server Component */}
-            <LeaderboardContents difficulty={searchParams.gameVersion} />
-          </table>
-        </div>
-      </main>
-    </div>
+    <Suspense fallback={<LoadingPokeball />}>
+      <div className="leaderboard-page" data-testid="leaderboard">
+        <Link className="nav-button-container" href="/">
+          <button className="nav-button">Home</button>
+        </Link>
+        <header>
+          <a href="https://www.youtube.com/watch?v=fCkeLBGSINs">
+            <img
+              className="pokemon-logo"
+              src={logo.src}
+              alt="The original Pokemon logo with a styled font, dark blue outline, and yellow fill."
+            />
+          </a>
+        </header>
+        <main className="leaderboard-contents">
+          <LeaderboardHeader />
+          <div className="leaderboard-score-table-container">
+            <table className="leaderboard-score-table">
+              <thead>
+                <tr>
+                  <th scope="col">Rank</th>
+                  <th scope="col">Trainer Name</th>
+                  <th scope="col">Time</th>
+                  <th scope="col">Favorite Pokemon</th>
+                  <th scope="col">Date</th>
+                </tr>
+              </thead>
+              {/* @ts-expect-error Server Component */}
+              <LeaderboardContents difficulty={searchParams.gameVersion} />
+            </table>
+          </div>
+        </main>
+      </div>
+    </Suspense>
   );
 }
