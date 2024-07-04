fb5f521bc93982169c44d6d5d9fa78d6bc2b75cb
Brian Wong <brianw7218@gmail.com>
2024-07-03 23:00:55 -0700

    Add back meta tag required for mobile responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
