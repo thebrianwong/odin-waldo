5c989a285ec029e01dd0b778b14abddd26429b49
Brian Wong <brianw7218@gmail.com>
2024-07-04 21:53:52 -0700

    Move score submission logic directly into the submit modal

--- src/components/SubmitScoreModal/type.ts
+++ src/components/SubmitScoreModal/type.ts
@@ -1,16 +1,9 @@
 import GameVersion from "../../types/gameVersion.type";
-import SubmissionResponse from "../../types/submissionResponse.type";
 
 type SubmitScoreModalProps = {
   timeScore: number;
   displayTime: string;
   gameVersion: GameVersion;
-  submitScore: (
-    timeInMilliseconds: number,
-    playerName: string,
-    playerFavoritePokemon: string,
-    gameVersion: GameVersion
-  ) => Promise<SubmissionResponse>;
   closeModal: () => void;
 };
 
