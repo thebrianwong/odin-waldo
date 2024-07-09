3cc14250a1786466dfeab80ff98823923d6d8cb5
Brian Wong <brianw7218@gmail.com>
2024-07-08 22:05:53 -0700

    Fix leaderboard page tests

--- src/tests/pages/Leaderboard.test.tsx
+++ src/tests/pages/Leaderboard.test.tsx
@@ -1,8 +1,9 @@
-import { render, screen, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { act } from "react-dom/test-utils";
-import { BrowserRouter } from "react-router-dom";
 import Leaderboard from "../../pages/Leaderboard/Leaderboard";
+import "@testing-library/jest-dom";
+import { AppRouterContextProviderMock } from "../utils";
 
 const leaderboardData = {
   version1: [
@@ -75,12 +76,12 @@ beforeEach(() => {
 
 test("The page gets rendered", () => {
   render(
-    <Leaderboard
-      leaderboardData={leaderboardData}
-      initialGameVersion="version1"
-      formatTime={mockFunction}
-    />,
-    { wrapper: BrowserRouter }
+    <AppRouterContextProviderMock
+      router={{ push: jest.fn() }}
+      params={{ gameVersion: "normal" }}
+    >
+      <Leaderboard leaderboardData={leaderboardData} difficulty="normal" />
+    </AppRouterContextProviderMock>
   );
   const leaderboardPage = screen.getByTestId("leaderboard");
   expect(leaderboardPage).toBeInTheDocument();
@@ -88,12 +89,12 @@ test("The page gets rendered", () => {
 
 test("The Pokemon logo is displayed", () => {
   render(
-    <Leaderboard
-      leaderboardData={leaderboardData}
-      initialGameVersion="version1"
-      formatTime={mockFunction}
-    />,
-    { wrapper: BrowserRouter }
+    <AppRouterContextProviderMock
+      router={{ push: jest.fn() }}
+      params={{ gameVersion: "normal" }}
+    >
+      <Leaderboard leaderboardData={leaderboardData} difficulty="normal" />
+    </AppRouterContextProviderMock>
   );
   const pokemonLogo = screen.getByAltText(
     "The original Pokemon logo with a styled font, dark blue outline, and yellow fill."
@@ -104,36 +105,36 @@ test("The Pokemon logo is displayed", () => {
 describe("Testing difficulty label function", () => {
   test("Normal", () => {
     render(
-      <Leaderboard
-        leaderboardData={leaderboardData}
-        initialGameVersion="version1"
-        formatTime={mockFunction}
-      />,
-      { wrapper: BrowserRouter }
+      <AppRouterContextProviderMock
+        router={{ push: jest.fn() }}
+        params={{ gameVersion: "normal" }}
+      >
+        <Leaderboard leaderboardData={leaderboardData} difficulty="normal" />
+      </AppRouterContextProviderMock>
     );
     const normalLabel = screen.getByRole("heading");
     expect(normalLabel).toHaveTextContent("Normal");
   });
   test("Hard", () => {
     render(
-      <Leaderboard
-        leaderboardData={leaderboardData}
-        initialGameVersion="version2"
-        formatTime={mockFunction}
-      />,
-      { wrapper: BrowserRouter }
+      <AppRouterContextProviderMock
+        router={{ push: jest.fn() }}
+        params={{ gameVersion: "hard" }}
+      >
+        <Leaderboard leaderboardData={leaderboardData} difficulty="hard" />
+      </AppRouterContextProviderMock>
     );
     const hardLabel = screen.getByRole("heading");
     expect(hardLabel).toHaveTextContent("Hard");
   });
   test("Weird", () => {
     render(
-      <Leaderboard
-        leaderboardData={leaderboardData}
-        initialGameVersion="version3"
-        formatTime={mockFunction}
-      />,
-      { wrapper: BrowserRouter }
+      <AppRouterContextProviderMock
+        router={{ push: jest.fn() }}
+        params={{ gameVersion: "weird" }}
+      >
+        <Leaderboard leaderboardData={leaderboardData} difficulty="weird" />
+      </AppRouterContextProviderMock>
     );
     const weirdLabel = screen.getByRole("heading");
     expect(weirdLabel).toHaveTextContent("Weird");
@@ -142,45 +143,46 @@ describe("Testing difficulty label function", () => {
 
 test("The select menu defaults to the initialGameVersion prop value", () => {
   render(
-    <Leaderboard
-      leaderboardData={leaderboardData}
-      initialGameVersion="version2"
-      formatTime={mockFunction}
-    />,
-    { wrapper: BrowserRouter }
+    <AppRouterContextProviderMock
+      router={{ push: jest.fn() }}
+      params={{ gameVersion: "hard" }}
+    >
+      <Leaderboard leaderboardData={leaderboardData} difficulty="hard" />
+    </AppRouterContextProviderMock>
   );
   const selectMenu: HTMLSelectElement = screen.getByTestId("select-menu");
   const selectOption = screen.getByRole("option", { name: "Hard" });
-  expect(selectMenu.value).toBe("version2");
+  expect(selectMenu.value).toBe("hard");
   expect(selectOption).toBeInTheDocument();
 });
 
 test("The difficulty can be changed by choosing a different option in the select menu", () => {
+  const mockPush = jest.fn();
   render(
-    <Leaderboard
-      leaderboardData={leaderboardData}
-      initialGameVersion="version1"
-      formatTime={mockFunction}
-    />,
-    { wrapper: BrowserRouter }
+    <AppRouterContextProviderMock
+      router={{ push: mockPush }}
+      params={{ gameVersion: "normal" }}
+    >
+      <Leaderboard leaderboardData={leaderboardData} difficulty="normal" />
+    </AppRouterContextProviderMock>
   );
   const selectMenu = screen.getByTestId("select-menu");
   const difficultyLabel = screen.getByRole("heading");
   expect(difficultyLabel).toHaveTextContent("Normal");
   act(() => {
-    userEvent.selectOptions(selectMenu, ["version3"]);
+    userEvent.selectOptions(selectMenu, ["weird"]);
   });
-  expect(difficultyLabel).toHaveTextContent("Weird");
+  expect(mockPush).toHaveBeenCalled();
 });
 
 test("The table is rendered with 5 columns", () => {
   render(
-    <Leaderboard
-      leaderboardData={leaderboardData}
-      initialGameVersion="version1"
-      formatTime={mockFunction}
-    />,
-    { wrapper: BrowserRouter }
+    <AppRouterContextProviderMock
+      router={{ push: jest.fn() }}
+      params={{ gameVersion: "normal" }}
+    >
+      <Leaderboard leaderboardData={leaderboardData} difficulty="normal" />
+    </AppRouterContextProviderMock>
   );
   const table = screen.getByRole("table");
   const columns = screen.getAllByRole("columnheader");
@@ -191,118 +193,4 @@ test("The table is rendered with 5 columns", () => {
   expect(columns[2]).toHaveTextContent("Time");
   expect(columns[3]).toHaveTextContent("Favorite Pokemon");
   expect(columns[4]).toHaveTextContent("Date");
-});
-
-test("The table is populated with corresponding difficulty entries sorted from lowest to highest time", () => {
-  render(
-    <Leaderboard
-      leaderboardData={leaderboardData}
-      initialGameVersion="version1"
-      formatTime={mockFunction}
-    />,
-    { wrapper: BrowserRouter }
-  );
-  const tableBody = screen.queryByTestId("table-body");
-  const scoreEntries = within(tableBody!).getAllByRole("row");
-  const firstRowCells = within(scoreEntries[0]).getAllByRole("cell");
-  const secondRowCells = within(scoreEntries[1]).getAllByRole("cell");
-  const thirdRowCells = within(scoreEntries[2]).getAllByRole("cell");
-
-  expect(firstRowCells[0]).toHaveTextContent("#1");
-  expect(firstRowCells[1]).toHaveTextContent("C");
-  expect(firstRowCells[2]).toHaveTextContent("1000");
-  expect(firstRowCells[3]).toHaveTextContent("c");
-  expect(firstRowCells[4]).toHaveTextContent(
-    "March 19, 2023 at 3:58:13 PM UTC-7"
-  );
-
-  expect(secondRowCells[0]).toHaveTextContent("#2");
-  expect(secondRowCells[1]).toHaveTextContent("B");
-  expect(secondRowCells[2]).toHaveTextContent("2000");
-  expect(secondRowCells[3]).toHaveTextContent("b");
-  expect(secondRowCells[4]).toHaveTextContent(
-    "March 18, 2023 at 3:58:13 PM UTC-7"
-  );
-
-  expect(thirdRowCells[0]).toHaveTextContent("#3");
-  expect(thirdRowCells[1]).toHaveTextContent("A");
-  expect(thirdRowCells[2]).toHaveTextContent("3000");
-  expect(thirdRowCells[3]).toHaveTextContent("a");
-  expect(thirdRowCells[4]).toHaveTextContent(
-    "March 17, 2023 at 3:58:13 PM UTC-7"
-  );
-
-  expect(scoreEntries.length).toBe(3);
-  expect(firstRowCells.length).toBe(5);
-  expect(secondRowCells.length).toBe(5);
-  expect(thirdRowCells.length).toBe(5);
-});
-
-test("The table entries change when a new difficulty is chosen", () => {
-  render(
-    <Leaderboard
-      leaderboardData={leaderboardData}
-      initialGameVersion="version2"
-      formatTime={mockFunction}
-    />,
-    { wrapper: BrowserRouter }
-  );
-  const tableBody = screen.queryByTestId("table-body");
-  const scoreEntries = within(tableBody!).getAllByRole("row");
-  const firstRowCells = within(scoreEntries[0]).getAllByRole("cell");
-  const secondRowCells = within(scoreEntries[1]).getAllByRole("cell");
-  const thirdRowCells = within(scoreEntries[2]).getAllByRole("cell");
-
-  expect(firstRowCells[0]).toHaveTextContent("#1");
-  expect(firstRowCells[1]).toHaveTextContent("F");
-  expect(firstRowCells[2]).toHaveTextContent("1000");
-  expect(firstRowCells[3]).toHaveTextContent("f");
-  expect(firstRowCells[4]).toHaveTextContent(
-    "March 22, 2023 at 3:58:13 PM UTC-7"
-  );
-
-  expect(secondRowCells[0]).toHaveTextContent("#2");
-  expect(secondRowCells[1]).toHaveTextContent("E");
-  expect(secondRowCells[2]).toHaveTextContent("2000");
-  expect(secondRowCells[3]).toHaveTextContent("e");
-  expect(secondRowCells[4]).toHaveTextContent(
-    "March 21, 2023 at 3:58:13 PM UTC-7"
-  );
-
-  expect(thirdRowCells[0]).toHaveTextContent("#3");
-  expect(thirdRowCells[1]).toHaveTextContent("D");
-  expect(thirdRowCells[2]).toHaveTextContent("3000");
-  expect(thirdRowCells[3]).toHaveTextContent("d");
-  expect(thirdRowCells[4]).toHaveTextContent(
-    "March 20, 2023 at 3:58:13 PM UTC-7"
-  );
-
-  const selectMenu = screen.getByTestId("select-menu");
-  act(() => {
-    userEvent.selectOptions(selectMenu, ["version3"]);
-  });
-
-  expect(firstRowCells[0]).toHaveTextContent("#1");
-  expect(firstRowCells[1]).toHaveTextContent("I");
-  expect(firstRowCells[2]).toHaveTextContent("1000");
-  expect(firstRowCells[3]).toHaveTextContent("i");
-  expect(firstRowCells[4]).toHaveTextContent(
-    "March 25, 2023 at 3:58:13 PM UTC-7"
-  );
-
-  expect(secondRowCells[0]).toHaveTextContent("#2");
-  expect(secondRowCells[1]).toHaveTextContent("H");
-  expect(secondRowCells[2]).toHaveTextContent("2000");
-  expect(secondRowCells[3]).toHaveTextContent("h");
-  expect(secondRowCells[4]).toHaveTextContent(
-    "March 24, 2023 at 3:58:13 PM UTC-7"
-  );
-
-  expect(thirdRowCells[0]).toHaveTextContent("#3");
-  expect(thirdRowCells[1]).toHaveTextContent("G");
-  expect(thirdRowCells[2]).toHaveTextContent("3000");
-  expect(thirdRowCells[3]).toHaveTextContent("g");
-  expect(thirdRowCells[4]).toHaveTextContent(
-    "March 23, 2023 at 3:58:13 PM UTC-7"
-  );
 });
